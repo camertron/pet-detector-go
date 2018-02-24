cae98db85e75e2c6b6f177be9104000fdbbf7fd4
Cameron Dutro <camertron@gmail.com>
2018-02-23 23:06:46 -0800

    Sigghhhh

--- graph.go
+++ graph.go
@@ -1,116 +1,113 @@
 package main
 
+// import "fmt"
+// import "github.com/davecgh/go-spew/spew"
+
 type Vertex struct {
-    id Entity
-    dist int
-    arcs map[Entity]int        // arcs[vertex id] = weight
+  value *Entity;
+  neighbors map[string]*Vertex;
 }
 
-type Candidates []Vertex
-func (h Candidates) Len() int           { return len(h) }
-func (h Candidates) Less(i, j int) bool { return h[i].dist < h[j].dist }
-func (h Candidates) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h Candidates) IsEmpty() bool      { return len(h) == 0 }
-
-func (h *Candidates) Push(v Vertex) {
-    var changed bool
-    old := *h
-    updated := *h
-    // insert Vertex at the correct position (keyed by distance)
-    for i, w := range old {
-        if v.id == w.id {
-            if changed {
-                if i + 1 < len(updated) {
-                    updated = append(updated[:i], updated[i+1:]...)
-                } else {
-                    updated = updated[:i]
-                }
-            } else if v.dist < w.dist {
-                updated[i] = v
-            }
-            changed = true
-        } else if v.dist < w.dist {
-            changed = true
-            updated = append(old[:i], v)
-            updated = append(updated, w)
-            updated = append(updated, old[i + 1:]...)
-        }
-    }
-    if !changed {
-        updated = append(old, v)
-    }
-    *h = updated
-}
-
-func (h *Candidates) Pop() (v Vertex) {
-    old := *h
-    v = old[0]
-    *h = old[1:]
-    return
+func (parent *Vertex) AddNeighbor(neighbor *Vertex) {
+  parent.neighbors[neighbor.value.Name] = neighbor;
 }
 
 type Graph struct {
-    visited map[Entity]bool
-    vertices map[Entity]Vertex
+  vertices map[string]*Vertex;
 }
 
-func NewGraph(vs map[Entity]Vertex) *Graph {
-    g := new(Graph)
-    g.visited = make(map[Entity]bool)
-    g.vertices = make(map[Entity]Vertex)
-    for i, v := range vs {
-        v.dist = 1000000
-        g.vertices[i] = v
-    }
-    return g
+func NewGraph() *Graph {
+  return &Graph{vertices: make(map[string]*Vertex)};
 }
 
-func (g *Graph) Len() int { return len(g.vertices)  }
-func (g *Graph) visit(v Entity) { g.visited[v] = true }
-
-func (g *Graph) ShortestPath(src, dest Entity) (x int) {
-    for k := range g.visited {
-        delete(g.visited, k)
-    }
-
-    for _, v := range g.vertices {
-        v.dist = 1000000
-    }
-
-    g.visit(src)
-    v := g.vertices[src]
-    h := make(Candidates, len(v.arcs))
-    // initialize the heap with out edges from src
-    for id, y := range v.arcs {
-        v := g.vertices[id]
-        // update the vertices being pointed to with the distance.
-        v.dist = y
-        g.vertices[id] = v
-        h.Push(v)
-    }
-    for src != dest {
-        if h.IsEmpty() {
-            return 1000000
-        }
-        v = h.Pop()
-        src = v.id
-        if g.visited[src] {
-            continue
-        }
-        g.visit(src)
-        for w, d := range v.arcs {
-            if g.visited[w] {
-                continue
-            }
-            c := g.vertices[w]
-            distance := d + v.dist
-            if distance < c.dist {
-                c.dist = distance
-                g.vertices[w] = c
-            }
-            h.Push(c)
-        }
-    }
-    v = g.vertices[dest]
-    return v.dist
+func (g *Graph) AddVertex(value *Entity) {
+  g.vertices[value.Name] = &Vertex{value: value, neighbors: make(map[string]*Vertex)};
 }
+
+func (g *Graph) AddEdge(value1 *Entity, value2 *Entity) {
+  g.vertices[value1.Name].AddNeighbor(g.vertices[value2.Name]);
+}
+
+func (g *Graph) ShortestPath(source, target *Entity) []*Entity {
+  distances := make(map[string]int);
+  previouses := make(map[string]*Vertex);
+
+  for name, _ := range g.vertices {
+    previouses[name] = nil;
+  }
+
+  distances[source.Name] = 0;
+  verts := make(map[string]*Vertex);
+
+  // copy vertex map
+  for name, vertex := range g.vertices {
+    verts[name] = vertex;
+  }
+
+  var nearestVertex *Vertex;
+
+  for len(verts) > 0 {
+    nearestVertex = nil;
+
+    for name, v := range verts {
+      if distance, ok := distances[name]; ok {
+        if nearestVertex == nil || distance < distances[nearestVertex.value.Name] {
+          nearestVertex = v;
+        }
+      }
+    }
+
+    if nearestVertex == nil {
+      break;
+    }
+
+    if _, ok := distances[nearestVertex.value.Name]; !ok {
+      break;
+    }
+
+    if target != nil && nearestVertex.value == target {
+      return g.composePath(target, distances[target.Name], previouses);
+    }
+
+    alt := distances[nearestVertex.value.Name] + 1;
+
+    for name, _ := range nearestVertex.neighbors {
+      if _, ok := distances[name]; !ok || alt < distances[name] {
+        distances[name] = alt;
+        previouses[name] = nearestVertex;
+      }
+    }
+
+    delete(verts, nearestVertex.value.Name);
+  }
+
+  return make([]*Entity, 0, 0);
+}
+
+func (g *Graph) composePath(target *Entity, distance int, previouses map[string]*Vertex) []*Entity {
+  result := make([]*Entity, distance);
+
+  for i := distance - 1; i >= 0; i -= 1 {
+    result[i] = target;
+    target = previouses[target.Name].value;
+  }
+
+  return result;
+}
+
+// func main() {
+//   first := Entity{Track: "top bottom", Name: "petTabby"};
+//   second := Entity{Track: "foo foo", Name: "track"};
+//   third := Entity{Track: "left right", Name: "houseTabby"};
+
+//   graph := NewGraph();
+//   graph.AddVertex(&first);
+//   graph.AddVertex(&second);
+//   graph.AddVertex(&third);
+//   graph.AddEdge(&first, &second);
+//   graph.AddEdge(&second, &third);
+
+//   path := graph.ShortestPath(&first, &third);
+
+//   spew.Dump(path);
+// }
--- pets.go
+++ pets.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 import "strings"
-import "strconv"
+// import "strconv"
 // import "github.com/davecgh/go-spew/spew"
 import "encoding/json"
 
-var data = `[[{"track":"bottom right","name":"houseCockatiel"},{"track":"bottom left right","name":"petSiamese"},{"track":"bottom left right","name":""},{"track":"bottom left","name":""}],[{"track":"top bottom right","name":"petTurtle"},{"track":"top left right","name":"petDacshund"},{"track":"top bottom left","name":"houseHedgehog"},{"track":"top bottom","name":""}],[{"track":"top bottom","name":""},{"track":"bottom right","name":"petTabby"},{"track":"top bottom left right","name":"petHusky"},{"track":"top bottom left","name":"petHedgehog"}],[{"track":"top bottom","name":"houseTabby"},{"track":"top bottom right","name":"car"},{"track":"top left right","name":""},{"track":"top bottom left","name":""}],[{"track":"top bottom","name":"petCockatiel"},{"track":"top right","name":""},{"track":"bottom left right","name":""},{"track":"top bottom left","name":"houseTurtle"}],[{"track":"top right","name":"houseSiamese"},{"track":"left right","name":"houseDacshund"},{"track":"top left right","name":""},{"track":"top left","name":"houseHusky"}]]`;
+var data = `[[{"track":"bottom right","name":"houseCockatiel"},{"track":"bottom left right","name":"petSiamese"},{"track":"bottom left right","name":"track-0"},{"track":"bottom left","name":"track-1"}],[{"track":"top bottom right","name":"petTurtle"},{"track":"top left right","name":"petDacshund"},{"track":"top bottom left","name":"houseHedgehog"},{"track":"top bottom","name":"track-2"}],[{"track":"top bottom","name":"track-3"},{"track":"bottom right","name":"petTabby"},{"track":"top bottom left right","name":"petHusky"},{"track":"top bottom left","name":"petHedgehog"}],[{"track":"top bottom","name":"houseTabby"},{"track":"top bottom right","name":"car"},{"track":"top left right","name":"track-4"},{"track":"top bottom left","name":"track-5"}],[{"track":"top bottom","name":"petCockatiel"},{"track":"top right","name":"track-6"},{"track":"bottom left right","name":"track-7"},{"track":"top bottom left","name":"houseTurtle"}],[{"track":"top right","name":"houseSiamese"},{"track":"left right","name":"houseDacshund"},{"track":"top left right","name":"track-8"},{"track":"top left","name":"houseHusky"}]]`;
 
 type Entity struct {
   Track string;
@@ -17,37 +17,33 @@ type Matrix [][]Entity;
 
 func main() {
   var matrix Matrix;
-  var vertices = make(map[Entity]Vertex);
   json.Unmarshal([]byte(data), &matrix);
 
-  for r := range matrix {
-    for c := range matrix[r] {
-      vertices[matrix[r][c]] = Vertex{id: matrix[r][c], dist: 0, arcs: make(map[Entity]int)};
-    }
-  }
+  graph := NewGraph();
 
   for r := range matrix {
     for c := range matrix[r] {
-      matrix.AddTopNeighbors(vertices, &matrix[r][c], c, r);
-      matrix.AddBottomNeighbors(vertices, &matrix[r][c], c, r);
-      matrix.AddLeftNeighbors(vertices, &matrix[r][c], c, r);
-      matrix.AddRightNeighbors(vertices, &matrix[r][c], c, r);
+      graph.AddVertex(&matrix[r][c])
+      matrix.AddLeftNeighbors(graph, &matrix[r][c], c, r);
+      matrix.AddRightNeighbors(graph, &matrix[r][c], c, r);
+      matrix.AddTopNeighbors(graph, &matrix[r][c], c, r);
+      matrix.AddBottomNeighbors(graph, &matrix[r][c], c, r);
     }
   }
 
-  var graph = NewGraph(vertices);
-  var distances = make(map[Entity]map[Entity]int);
+  var distances = make(map[string]map[string]int);
 
-  for fromEntity, _ := range graph.vertices {
-    for toEntity, _ := range graph.vertices {
-      if fromEntity != toEntity && fromEntity.Name != "" && toEntity.Name != "" {
-        if distances[fromEntity] == nil {
-          distances[fromEntity] = make(map[Entity]int)
+  for fromName, fromVertex := range graph.vertices {
+    for toName, toVertex := range graph.vertices {
+      if fromVertex != toVertex && !strings.Contains(fromName, "track") && !strings.Contains(toName, "track") {
+        if distances[fromName] == nil {
+          distances[fromName] = make(map[string]int)
         }
 
-        var distance = graph.ShortestPath(fromEntity, toEntity)
-        fmt.Println(fromEntity.Name + " -> " + toEntity.Name + " = " + strconv.Itoa(distance));
-        distances[fromEntity][toEntity] = distance;
+        shortestPath := graph.ShortestPath(fromVertex.value, toVertex.value);
+        fmt.Println(shortestPath);
+        // fmt.Println(fromEntity.Name + " -> " + toEntity.Name + " = " + strconv.Itoa(distance));
+        // distances[fromEntity][toEntity] = distance;
       }
     }
   }
@@ -63,50 +59,54 @@ func (m *Matrix) height() int {
   return len(*m);
 }
 
-func (mRef *Matrix) AddTopNeighbors(vertices map[Entity]Vertex, entity *Entity, x, y int) {
+func (mRef *Matrix) AddTopNeighbors(graph *Graph, entity *Entity, x, y int) {
   var m = *mRef;
 
   if strings.Contains(entity.Track, "top") {
     neighbor := m.TopNeighbor(x, y);
 
     if neighbor != nil {
-      vertices[*entity].arcs[*neighbor] = 1;
+      graph.AddVertex(neighbor);
+      graph.AddEdge(entity, neighbor);
     }
   }
 }
 
-func (mRef *Matrix) AddBottomNeighbors(vertices map[Entity]Vertex, entity *Entity, x, y int) {
+func (mRef *Matrix) AddBottomNeighbors(graph *Graph, entity *Entity, x, y int) {
   var m = *mRef;
 
   if strings.Contains(entity.Track, "bottom") {
     neighbor := m.BottomNeighbor(x, y);
 
     if neighbor != nil {
-      vertices[*entity].arcs[*neighbor] = 1;
+      graph.AddVertex(neighbor);
+      graph.AddEdge(entity, neighbor);
     }
   }
 }
 
-func (mRef *Matrix) AddLeftNeighbors(vertices map[Entity]Vertex, entity *Entity, x, y int) {
+func (mRef *Matrix) AddLeftNeighbors(graph *Graph, entity *Entity, x, y int) {
   var m = *mRef;
 
   if strings.Contains(entity.Track, "left") {
     neighbor := m.LeftNeighbor(x, y);
 
     if neighbor != nil {
-      vertices[*entity].arcs[*neighbor] = 1;
+      graph.AddVertex(neighbor);
+      graph.AddEdge(entity, neighbor);
     }
   }
 }
 
-func (mRef *Matrix) AddRightNeighbors(vertices map[Entity]Vertex, entity *Entity, x, y int) {
+func (mRef *Matrix) AddRightNeighbors(graph *Graph, entity *Entity, x, y int) {
   var m = *mRef;
 
   if strings.Contains(entity.Track, "right") {
     neighbor := m.RightNeighbor(x, y);
 
     if neighbor != nil {
-      vertices[*entity].arcs[*neighbor] = 1;
+      graph.AddVertex(neighbor);
+      graph.AddEdge(entity, neighbor);
     }
   }
 }
